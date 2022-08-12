e01524ee57ce522fb15eeb0cbd191a8cc1e77dc5
Paul Lynch <paul.lynch@wilsonsecurity.com.au>
2022-08-12 15:47:35 +1000

    Adding routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import './App.css'
 import './assets/style/app.scss'
-import { useCallback } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { closeActionModal } from './redux/peopleSlice'
-import { isActionModalOpen, getActiverPerson } from './redux/selectors'
-import { PeopleGrid } from './components/peopleGrid'
-import { ActionModal } from './components/actionModal'
+import { Routes, Route } from 'react-router-dom'
+import ListPeople from './pages/listPeople'
 
 function App() {
-  const dispatch = useDispatch()
-  const isModalOpen = useSelector(isActionModalOpen)
-  const activePerson = useSelector(getActiverPerson)
-
-  const closeModal = useCallback(() => {
-    dispatch(closeActionModal())
-  }, [])
-
   return (
     <div className="App">
       <h1>Paul's Vite Test</h1>
-      <PeopleGrid />
-      <ActionModal isOpen={isModalOpen} closeFn={closeModal} data={activePerson} />
+      <Routes>
+        <Route path="/" element={<ListPeople />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/listPeople.tsx
@@ -0,0 +1,27 @@
+import { useCallback } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { closeActionModal } from '../redux/peopleSlice'
+import { isActionModalOpen, getActiverPerson } from '../redux/selectors'
+import { PeopleGrid } from '../components/peopleGrid'
+import { ActionModal } from '../components/actionModal'
+
+const ListPeople = () => {
+  const dispatch = useDispatch()
+  const isModalOpen = useSelector(isActionModalOpen)
+  const activePerson = useSelector(getActiverPerson)
+
+  const closeModal = useCallback(() => {
+    dispatch(closeActionModal())
+  }, [])
+
+  return (
+    <>
+        <div className="page">
+        <PeopleGrid />
+        </div>
+        <ActionModal isOpen={isModalOpen} closeFn={closeModal} data={activePerson} />
+    </>
+  )
+}
+
+export default ListPeople
