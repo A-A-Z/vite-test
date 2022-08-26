ecd8f6900b840e3d92a07f49d61d169ac9296b81
Paul Lynch <paul.lynch@wilsonsecurity.com.au>
2022-08-26 15:55:12 +1000

    Block lastpass icon from fields

--- src/components/filterText.tsx
+++ src/components/filterText.tsx
@@ -1,11 +1,8 @@
-
 interface FilterTextProps {
     onChangeFn: Function
 }
 
 export const FilterText = ({ onChangeFn }: FilterTextProps) => {
     const onChangeCall = (e: React.ChangeEvent<HTMLInputElement>) => onChangeFn(e.target.value)
-    return (
-            <input type="text" className="filter__input" onChange={onChangeCall} />
-    )
+    return <input type="text" className="filter__input" onChange={onChangeCall} data-lpignore="true" />
 }
--- src/components/inputText.tsx
+++ src/components/inputText.tsx
@@ -3,5 +3,5 @@ import { useFormContext } from 'react-hook-form'
 
 export const InputText = ({ id, name }: InputProps) => {
     const { register } = useFormContext()
-    return <input id={id} className="field__input field__input--text" {...register(name)} />
+    return <input id={id} className="field__input field__input--text" data-lpignore="true" {...register(name)} />
 }
